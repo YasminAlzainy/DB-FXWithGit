a6508173410b8107b525cf57f169efc9c3f8a951
Yasmin <Yasmin.alzainy@gmail.com>
2019-04-06 15:58:46 +0200

    adding User class , UserDAO

--- GitWithDB/src/main/java/com/mycompany/gitwithdb/User.java
+++ GitWithDB/src/main/java/com/mycompany/gitwithdb/User.java
@@ -10,5 +10,67 @@ package com.mycompany.gitwithdb;
  * @author yasmin
  */
 public class User {
+    private int id , phone;
+    private String firstName , midName , lastName, email;
+
+    public User(int id, int phone, String firstName, String midName, String lastName, String email) {
+        this.id = id;
+        this.phone = phone;
+        this.firstName = firstName;
+        this.midName = midName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMidName() {
+        return midName;
+    }
+
+    public void setMidName(String midName) {
+        this.midName = midName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
     
 }
--- GitWithDB/src/main/java/com/mycompany/gitwithdb/UserDAO.java
+++ GitWithDB/src/main/java/com/mycompany/gitwithdb/UserDAO.java
@@ -1,14 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.gitwithdb;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
 
 /**
  *
  * @author yasmin
  */
 public class UserDAO {
-    
+
+    private DataSource dataSource = null;
+    private Connection connection = null;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
+    private boolean flag = false;
+    private User user;
+
+    public UserDAO() {
+        try {
+            user = new User();
+            dataSource = DBProperties.getMySQLDataSource();
+            connection = dataSource.getConnection();
+            String query = "select * from user";
+            preparedStatement = connection.prepareStatement(query, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            resultSet = preparedStatement.executeQuery();
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public User newUser() {
+        return null;
+    }
+
+    public User updateUser() {
+        return null;
+    }
+
+    public User deleteUser() {
+        return null;
+    }
+
+    public User firstUser() {
+        return null;
+    }
+
+    public User lastUser() {
+        return null;
+    }
+
+    public User prevUser() {
+        return null;
+    }
+
+    public User nextUser() {
+        return null;
+    }
+
 }
